20a50363163af761931e19ac98484f8f2f0437fd
FossenWang <31503932+FossenWang@users.noreply.github.com>
2017-10-20 14:41:23 +0800

    ajust database 2
    ajust exercise
    add my own customized training program

--- app/src/main/res/raw/training_program.sql
+++ app/src/main/res/raw/training_program.sql
@@ -1,4 +1,50 @@
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS `tp1508060008889`;
+CREATE TABLE IF NOT EXISTS `tp1508060008889` (
+	`day`	INTEGER,
+	`number`	INTEGER,
+	`exercise`	TEXT,
+	`sets`	INTEGER,
+	`reps`	TEXT,
+	`rest`	INTEGER,
+	`structure`	TEXT
+);
+INSERT INTO `tp1508060008889` VALUES (1,1,'卧推,哑铃卧推',5,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (1,2,'上斜哑铃卧推,上斜卧推',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (1,3,'上斜绳索夹胸,上斜单臂夹胸',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (1,4,'绳索下压,直杆下压',5,'8~12',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (1,5,'俯身臂屈伸,仰卧臂屈伸',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (1,6,'哑铃侧平举,拉力器侧平举',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (2,1,'硬拉',5,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (2,2,'阔背下拉',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (2,3,'单臂哑铃划船,杠铃划船',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (2,4,'哑铃弯举,杠铃弯举,拉力器弯举',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (2,5,'绳索锤式弯举,锤式弯举',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (2,6,'开肘哑铃划船,绳索面拉',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (3,1,'深蹲',5,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (3,2,'器械腿推',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (3,3,'哑铃弓步,保加利亚分腿蹲',3,'8~12',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (3,4,'负重提踵',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (3,5,'杠铃肩推,哑铃肩推',5,'8~12',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (3,6,'哑铃侧平举,拉力器侧平举',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (4,1,'卧推,哑铃卧推',5,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (4,2,'下斜哑铃卧推,下斜卧推',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (4,3,'下斜绳索夹胸,下斜单臂夹胸',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (4,4,'窄距卧推',5,'8~12',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (4,5,'绳索下压,直杆下压',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (4,6,'哑铃侧平举,拉力器侧平举',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (5,1,'硬拉',5,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (5,2,'阔背下拉',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (5,3,'直臂下压，单手直臂下拉',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (5,4,'杠铃弯举,哑铃弯举,拉力器弯举',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (5,5,'锤式弯举,绳索锤式弯举',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (5,6,'绳索面拉,开肘哑铃划船',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (6,1,'深蹲',5,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (6,2,'保加利亚分腿蹲,哑铃弓步',3,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (6,3,'哑铃卧推,卧推',5,'8~12',120,'normal');
+INSERT INTO `tp1508060008889` VALUES (6,4,'绳索夹胸,单臂夹胸,哑铃飞鸟,仰卧直臂上拉',3,'10~15',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (6,5,'哑铃肩推,杠铃肩推',3,'8~12',60,'normal');
+INSERT INTO `tp1508060008889` VALUES (6,6,'哑铃侧平举,拉力器侧平举',3,'10~15',60,'normal');
 DROP TABLE IF EXISTS `tp1508060008888`;
 CREATE TABLE IF NOT EXISTS `tp1508060008888` (
 	`day`	INTEGER,
@@ -137,8 +183,9 @@ CREATE TABLE IF NOT EXISTS `program_list` (
 	`note`	TEXT
 );
 INSERT INTO `program_list` VALUES ('tp1508060008886','五分化训练','增肌','胸,背,腿,肩,臂,休息,休息',0,20170101,'五分化训练的设计原理，是每天针对5大肌肉群 - 腿、胸、背、肩、臂 - 中的一项，以最大化地施加超负荷刺激。以5天为一训练周期，之后休息1-2天。因此，当以周为单位时，参考方案为：\n- 周一：腿\n- 周二：胸\n- 周三：背\n- 周四：肩\n- 周五：臂\n- 周六：休息\n- 周日：休息');
-INSERT INTO `program_list` VALUES ('tp1508060008887','腿推拉训练','增肌','腿,推,拉,腿,推,拉,休息',0,20170101,'腿推拉训练设计的设计原理，是以肌肉的三大功能 - 即腿、推、拉 - 进行训练内容划分。以3天为一训练周期，之后休息1天，或两周期循环后再休息。因此，当以周为单位时，参考方案为：\n- 周一：腿（A）\n- 周二：推（A）\n- 周三：拉（A）\n- 周四：腿（B）\n- 周五：推（B）\n- 周六：拉（B）\n- 周日：休息');
+INSERT INTO `program_list` VALUES ('tp1508060008887','腿推拉训练','增肌','腿,推,拉,腿,推,拉,休息',0,20170101,'腿推拉训练设计的设计原理，是以肌肉的三大功能 - 即腿、推、拉 - 进行训练内容划分。以3天为一训练周期，之后休息1天，或两周期循环后再休息。因此，当以周为单位时，参考方案为：\n- 周一：腿\n- 周二：推\n- 周三：拉\n- 周四：腿\n- 周五：推\n- 周六：拉\n- 周日：休息');
 INSERT INTO `program_list` VALUES ('tp1508060008888','全身式训练','保持','休息,休息,休息,休息,全身,休息,全身',0,20170101,'全身式训练设计，是针对忙碌健身者，最为高效省时间的训练方案。它的设计原理，是每天锻炼全身所有主要肌肉群 - 多动作，少组数。\n全身式训练设计，1天就是一个训练周期，之后需要休息1-2天。因此，对于忙碌的学生或白领，如果只有周末有空，参考方案为：\n- 周一：学校/工作\n- 周二：学校/工作\n- 周三：学校/工作\n- 周四：学校/工作\n- 周五：全身\n- 周六：休息\n- 周日：全身');
+INSERT INTO `program_list` VALUES ('tp1508060008889','Fossen的胸背腿训练','增肌','胸,背,腿,胸,背,腿,休息',0,20170101,'以推拉腿三分化训练为基本模板，针对我自身的训练需求做出调整，制定了我个人的训练方案。该方案主要按胸、背、腿三个大肌群划分，再辅以锻炼小肌群的动作，三天一小循环，两个小循环休息一天。\n现阶段为了更好的锻炼胸部，我在每周最后一天练腿日中也加入了两组胸部训练动作。每日的训练部位如下：\n第一天练胸、肱三、肩；\n第二天练背、肱二、三角肌后束；\n第三天练下肢、肩；\n第四天练胸、肱三、肩；\n第五天练背、肱二、三角肌后束；\n第六天练下肢、胸、肩；');
 DROP TABLE IF EXISTS `log_catalog`;
 CREATE TABLE IF NOT EXISTS `log_catalog` (
 	`date`	INTEGER,
